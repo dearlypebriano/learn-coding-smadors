196bde9c9be4a40255e4972c3578b484d1ca46b6
dearly <dearly@MacBook-Air-Dearly.local>
2024-05-02 22:29:39 +0700

    Update input user + penjelasan detail💻

--- input.cpp
+++ input.cpp
@@ -1,37 +1,39 @@
-#include <iostream>
-#include <sstream>
+#include <iostream> // header file untuk input output
+#include <sstream>  // header file untuk istringstream
 
 int main() {
-    char name[100], alamat[100];
-    int kelas;
-    std::string input, whole_input;
+    char name[100];  // variable untuk menyimpan nama
+    char alamat[100]; // variable untuk menyimpan alamat
+    int kelas; // variable untuk menyimpan kelas
+    std::string input; // variable untuk menyimpan input dari user
+    std::string whole_input; // variable untuk menyimpan seluruh input
 
-    printf("Masukkan nama: ");
-    getline(std::cin, input);
-    whole_input += "Nama: " + input + "\n";
-    std::istringstream iss_name(input);
+    printf("Masukkan nama: "); // output ke layar
+    getline(std::cin, input); // mengambil input dari user dan disimpan di variable input
+    whole_input += "Nama: " + input + "\n"; // menambahkan input nama ke variable whole_input
+    std::istringstream iss_name(input); // membuat istringstream untuk memproses input nama
 
-    iss_name >> name;
-    iss_name.str(std::string());
-    iss_name.clear();
+    iss_name >> name; // membaca input nama dan disimpan di variable name
+    iss_name.str(std::string()); // mengosongkan istringstream
+    iss_name.clear(); // me-clear stream, agar bisa digunakan lagi
 
-    printf("Masukkan alamat: ");
-    getline(std::cin, input);
-    whole_input += "Alamat: " + input + "\n";
-    std::istringstream iss_alamat(input);
+    printf("Masukkan alamat: "); // output ke layar
+    getline(std::cin, input); // mengambil input dari user dan disimpan di variable input
+    whole_input += "Alamat: " + input + "\n"; // menambahkan input alamat ke variable whole_input
+    std::istringstream iss_alamat(input); // membuat istringstream untuk memproses input alamat
 
-    iss_alamat >> alamat;
-    iss_alamat.str(std::string());
-    iss_alamat.clear();
+    iss_alamat >> alamat; // membaca input alamat dan disimpan di variable alamat
+    iss_alamat.str(std::string()); // mengosongkan istringstream
+    iss_alamat.clear(); // me-clear stream, agar bisa digunakan lagi
 
-    printf("Masukkan kelas: ");
-    scanf("%d", &kelas);
-    whole_input += "Kelas: " + std::to_string(kelas) + "\n";
+    printf("Masukkan kelas: "); // output ke layar
+    scanf("%d", &kelas); // mengambil input dari user dan disimpan di variable kelas
+    whole_input += "Kelas: " + std::to_string(kelas) + "\n"; // menambahkan input kelas ke variable whole_input
 
-    printf("%s", whole_input.c_str());
+    printf("%s", whole_input.c_str()); // mencetak seluruh input yang sudah diproses
 
-    printf("Hello %s, alamat %s, kelas %d\n", name, alamat, kelas);
+    printf("Hello %s, alamat %s, kelas %d\n", name, alamat, kelas); // mencetak seluruh input yang sudah diproses
 
-    return 0;
+    return 0; // mengembalikan 0 sebagai exit code program
 }
 
